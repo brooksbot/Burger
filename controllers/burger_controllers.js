397baca487b9bd5dfba56d0dda2c427fa5d4d712
Brooksbot <chrisbailey@Chriss-MacBook-Air.local>
2018-08-15 00:19:10 -0500

    mysql db, orm, controller

---
+++ controllers/burger_controllers.js
@@ -0,0 +1,33 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// get route -> index
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  burger.all(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
+  });
+});
+
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+// put route -> back to index
+router.put("/burgers/update", function(req, res) {
+  burger.update(req.body.burger_id, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
