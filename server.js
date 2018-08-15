386120a67941d312981bc31a8298c7aceeb4a1a7
Brooksbot <chrisbailey@Chriss-MacBook-Air.local>
2018-08-15 00:01:37 -0500

    server.js, handlebars, others

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+// parse application
+app.use(bodyParser.urlencoded({ extended: false }));
+// override with POST 
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on port:", PORT);
+});
